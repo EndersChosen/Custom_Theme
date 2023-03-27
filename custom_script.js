e965c9f0b45eede7c9a66625978e6eddfb4e24b0
Caleb Kruger <enderschosen@gmail.com>
2023-03-26 22:42:56 -0600

    started working on udpateModuleProgress()

--- custom_script.js
+++ custom_script.js
@@ -9,6 +9,7 @@
             const button = document.createElement('button');
             button.innerText = 'Update Module Progress';
             button.style = "margin-bottom: 5px;";
+            button.addEventListener('click', updateModuleProgress);
 
             gradebook.append(button);
         })
@@ -24,4 +25,24 @@
                 waitForElement(selector, callback);
             }, 500);
     }
+
+    async function updateModuleProgress() {
+        const courseID = window.location.pathname.match(/courses\/([0-9]+)/)[1];
+
+        let userList = await getUsers(courseID);
+
+    }
+
+    async function getUsers(courseID) {
+        const response = await fetch(`https://${window.location.host}/api/v1/courses/${courseID}/users?per_page=100&include[]=enrollments`);
+
+        let nextPage = isNext(response.headers.get('link'));
+
+        // https://ckruger.instructure.com/api/v1/courses/2155/modules?student_id=13320
+    }
+
+    function isNext(headers) {
+
+    }
+
 })();
